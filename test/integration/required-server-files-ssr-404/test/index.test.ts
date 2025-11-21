e70fa9512af029833879f7e956a69d781c7a703a
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-21 14:50:53 +0100

    update tests

--- test/integration/required-server-files-ssr-404/test/index.test.ts
+++ test/integration/required-server-files-ssr-404/test/index.test.ts
@@ -35,6 +35,7 @@ describe('Required Server Files', () => {
           if (
             file === 'server' ||
             file === 'required-server-files.json' ||
+            file === 'node_modules' ||
             requiredFilesManifest.files.includes(join('.next', file))
           ) {
             continue
