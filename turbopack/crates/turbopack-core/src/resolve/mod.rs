b380a5d38945de1077b8f1f42028f3f0530760d6
Niklas Mischkulnig <4586894+mischnic@users.noreply.github.com>
2025-11-24 16:51:53 +0100

    Make target the full path

--- turbopack/crates/turbopack-core/src/resolve/mod.rs
+++ turbopack/crates/turbopack-core/src/resolve/mod.rs
@@ -414,6 +414,8 @@ pub enum ResolveResultItem {
         name: RcStr,
         ty: ExternalType,
         traced: ExternalTraced,
+        /// The file path to the resolved file. Passing a value will create a symlink in the output
+        /// root to be able to access potentially transitive dependencies.
         target: Option<FileSystemPath>,
     },
     Ignore,
